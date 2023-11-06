bdfe7463d78653c1c13c1500d27187d901c5d921
Phien Nguyen <nguyenphien17@yahoo.com>
2023-11-05 21:00:57 -0800

    chore: added one more table

--- tpch.sql
+++ tpch.sql
@@ -7,4 +7,19 @@ CREATE TABLE player (
     player_draft TEXT
 );
 
+CREATE TABLE player_stats (
+    ps_player_id INTEGER,
+    ps_season_id INTEGER,
+    ps_team_id INTEGER,
+    ps_games INTEGER,
+    ps_pts DECIMAL,
+    ps_trb DECIMAL,
+    ps_ast DECIMAL,
+    ps_fg DECIMAL,
+    ps_fg3 DECIMAL,
+    ps_ft DECIMAL,
+    ps_efg DECIMAL
+);
+
 .import data/player.db player
+.import data/player_stats.db player_stats
