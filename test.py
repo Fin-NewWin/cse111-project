a370c1a5625173aea8b8d7fe28365d9c4f3ced7f
dustinvy <123767938+dustinvy@users.noreply.github.com>
2023-12-04 15:56:56 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import sqlite3
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 def check_tab_exist(cur: sqlite3.Cursor, table: str):
     sql = f"""
@@ -27,8 +29,8 @@ def test_table(cur: sqlite3.Cursor, table: str):
     cur.execute(sql)
     rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    #for row in rows:
+    #   print(row)
 
 
 def create_table(cur: sqlite3.Cursor, table: str, table_attr: str):
@@ -182,57 +184,20 @@ def main():
     conn.close()
 
 
-# def display_menu():
-#     print("\nMenu:")
-#     print("1. View Player Information")
-#     print("2. View Team Information")
-#     print("3. View Season Information")
-#     print("4. Exit")
+def connect_db():
+    return sqlite3.connect('tpch.sqlite')
 
 
-# def view_player_information():
-#     cur.execute("SELECT * FROM team")
-#     players = cur.fetchall()
-
-#     print("\nPlayer Information:")
-#     for player in players:
-#         print(player)
-
-
-# def view_team_information():
-#     cur.execute("SELECT * FROM team")
-#     teams = cur.fetchall()
-
-#     print("\nTeam Information:")
-#     for team in teams:
-#         print(team)
-
-
-# def view_season_information():
-#     cur.execute("SELECT * FROM season")
-#     seasons = cur.fetchall()
-
-#     print("\nSeason Information:")
-#     for season in seasons:
-#         print(season)
-
-
-# while True:
-#     display_menu()
-#     choice = input("Enter the number of your choice: ")
-
-#     if choice == "1":
-#         view_player_information()
-#     elif choice == "2":
-#         view_team_information()
-#     elif choice == "3":
-#         view_season_information()
-#     elif choice == "4":
-#         print("Exiting the program. Goodbye!")
-#         break
-#     else:
-#         print("Please enter a valid number.")
+@app.route('/')
+def index():
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM player")
+    data = cur.fetchall()
+    conn.close()
+    return render_template('index.html', data=data)
 
 
 if __name__ == "__main__":
+    app.run(debug=True)
     main()
