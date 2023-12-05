c01ac13648190fe242c556f1b81b8302f9cb8af9
dustinvy <123767938+dustinvy@users.noreply.github.com>
2023-12-04 21:17:47 -0800

    updated

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ def test_table(cur: sqlite3.Cursor, table: str):
     cur.execute(sql)
     rows = cur.fetchall()
 
-    # for row in rows:
+    #for row in rows:
     #   print(row)
 
 
@@ -140,8 +140,8 @@ def create_tab(conn: sqlite3.Connection):
         "season": """
         CREATE TABLE IF NOT EXISTS season (
             season_id INTEGER,
-            season_mvp INTEGER,
             season_team_id INTEGER,
+            season_mvp INTEGER,
             season_rookie INTEGER,
             season_point INTEGER,
             season_rbs INTEGER,
@@ -185,21 +185,19 @@ def main():
     conn.close()
 
 
-@app.route("/")
+@app.route('/')
 def index():
     conn = sqlite3.connect(r"tpch.sqlite")
     cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT player_id, player_name, player_position, player_height,
+    cur.execute("""
+        SELECT player_id, player_name, player_position, player_height, 
                player_weight, player_draft, team_name
         FROM player, team
         WHERE player_team_id = team_id
-    """
-    )
+    """)
     data = cur.fetchall()
     conn.close()
-    return render_template("index.html", data=data)
+    return render_template('index.html', data=data)
 
 
 @app.route("/player/<category>/<int:sort>")
@@ -224,69 +222,83 @@ def player(category, sort):
     return render_template("index.html", data=data, sign=sort, cat=category)
 
 
-@app.route("/player_stats/<int:player_id>/<player_name>")
+@app.route('/player_stats/<int:player_id>/<player_name>')
 def player_stats(player_id, player_name):
     conn = sqlite3.connect(r"tpch.sqlite")
     cur = conn.cursor()
 
-    # Fetch player stats and team name using a JOIN
-    cur.execute(
-        """
+    cur.execute("""
         SELECT *, team_name
-        FROM player_stats, team
+        FROM player_stats, team 
         WHERE ps_team_id = team_id
               AND ps_player_id = ?
-    """,
-        (player_id,),
-    )
+    """, (player_id,))
     player_stats_data = cur.fetchall()
 
     conn.close()
 
-    return render_template(
-        "player_stats.html",
-        player_stats_data=player_stats_data,
-        player_name=player_name,
-    )
+    return render_template('player_stats.html', player_stats_data=player_stats_data, player_name=player_name)
 
 
-@app.route("/team_info/<int:team_id>")
+@app.route('/team_info/<int:team_id>')
 def team_info(team_id):
     conn = sqlite3.connect(r"tpch.sqlite")
     cur = conn.cursor()
 
     # Fetch basic team information with coach name
-    cur.execute(
-        """
+    cur.execute("""
         SELECT team.*, coach.coach_name, state.state_name
         FROM team
         JOIN coach ON team.team_coach = coach.coach_id
         JOIN state ON team.team_state_id = state.state_id
         WHERE team_id = ?
-    """,
-        (team_id,),
-    )
+    """, (team_id,))
     team_info_data = cur.fetchone()
 
     # Fetch team_season data
-    cur.execute(
-        """
+    cur.execute("""
         SELECT * FROM team_season
         WHERE ts_team_id = ?
-    """,
-        (team_id,),
-    )
+    """, (team_id,))
     team_season_data = cur.fetchall()
 
     conn.close()
 
-    return render_template(
-        "team_info.html",
-        team_info_data=team_info_data,
-        team_season_data=team_season_data,
-    )
+    return render_template('team_info.html', team_info_data=team_info_data, team_season_data=team_season_data)
+
+
+@app.route('/season_accolades/<int:season_id>')
+def season_accolades(season_id):
+    conn = sqlite3.connect(r"tpch.sqlite")
+    cur = conn.cursor()
+
+    # Fetch accolades for the selected season including player names and team names
+    cur.execute("""
+        SELECT season.*,
+               team_champion.team_name AS champion_name,
+               mvp.player_name AS mvp_name,
+               rookie.player_name AS rookie_name,
+               points_leader.player_name AS points_leader_name,
+               rebounds_leader.player_name AS rebounds_leader_name,
+               assists_leader.player_name AS assists_leader_name,
+               win_share.player_name AS win_share_name
+        FROM season
+        LEFT JOIN team AS team_champion ON season.season_team_id = team_champion.team_id
+        LEFT JOIN player AS mvp ON season.season_mvp = mvp.player_id
+        LEFT JOIN player AS rookie ON season.season_rookie = rookie.player_id
+        LEFT JOIN player AS points_leader ON season.season_point = points_leader.player_id
+        LEFT JOIN player AS rebounds_leader ON season.season_rbs = rebounds_leader.player_id
+        LEFT JOIN player AS assists_leader ON season.season_ast = assists_leader.player_id
+        LEFT JOIN player AS win_share ON season.season_wins = win_share.player_id
+        WHERE season_id = ?
+    """, (season_id,))
+    season_accolades_data = cur.fetchone()
+
+    conn.close()
+
+    return render_template('season_accolades.html', season_accolades_data=season_accolades_data)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
     main()
+    app.run(debug=True)
